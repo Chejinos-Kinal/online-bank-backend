af85446892d841c1345a38ed60eb6c3f9a14418c
AnderCabrera <andercabrerac@gmail.com>
2024-06-07 11:51:06 -0600

    fixing products routes, product model and encrypt of password in default-data

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -2,36 +2,40 @@
 
 import { Schema, model } from 'mongoose';
 
-const productSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    price: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    timesSold: {
+      type: Number,
+      default: 0,
+      required: false,
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: false,
+  {
+    timestamps: true,
+    versionKey: false,
   },
-  price: {
-    type: Number,
-    min: 0,
-    required: true,
-  },
-  stock: {
-    type: Number,
-    min: 0,
-    required: true,
-  },
-  timesSold: {
-    type: Number,
-    default: 0,
-    required: false,
-  },
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Category',
-    required: true,
-  },
-  timestamps: true,
-  versionKey: false,
-});
+);
 export default model('Product', productSchema);
