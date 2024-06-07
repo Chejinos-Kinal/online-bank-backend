1cbdaff67a9498812b4ea03bfd421f217f2fca56
Ander Cabrera <74786994+AnderCabrera@users.noreply.github.com>
2024-06-07 12:17:34 -0600

    removing updateMovements commented function - movements.controller.js

--- src/controllers/movements.controller.js
+++ src/controllers/movements.controller.js
@@ -38,63 +38,6 @@ export const addMovement = async (req, res) => {
   }
 };
 
-/* export const updateMovement = async (req, res) => {
-    try {
-        let data = req.body;
-        let { id } = req.params;
-        let userId = req.user._id;
-
-        let user = await User.findById(userId);
-
-         let accountToDeposit = await Account.findOne({ accountNumber: data.numberAccountDestination }); 
-
-        if (user.role !== 'ADMIN') return res.status(401).send({ message: 'You cannot update the movements, please contact an admin to help you.' });
-
-        let existingMovement = await Movement.findById(id);
-        if (!existingMovement) return res.status(404).send({ message: 'Movement not found' });
-
-        let updateMovement = await Movement.findOneAndUpdate(
-            { _id: id },
-            data,
-            { new: true }
-        );
-
-        if (!updateMovement) return res.status(404).send({ message: 'Movement not found, not updated.' });
-
-        if ( data.numberAccountDestination != null ||  data.amount != null) {
-            if (!accountToDeposit) {
-                return res.status(404).send({ message: 'Account to deposit not found' });
-            } 
-            let resetBalance = 0; // Assuming resetBalance is the value to reset the balance to zero before adding the amount
-            if (accountToDeposit.accountNumber === existingMovement.numberAccountDestination) {
-                await Account.findOneAndUpdate(
-                    { accountNumber: data.numberAccountDestination },
-                    { balance: resetBalance }, // Resetting balance to zero before adding the new amount
-                    { new: true }
-                );
-                await Account.findOneAndUpdate(
-                    { accountNumber: data.numberAccountDestination },
-                    { $inc: { balance: data.amount } }, // Incrementing the balance by the new amount
-                    { new: true }
-                );
-            } else {
-                await Account.findOneAndUpdate(
-                    { accountNumber: data.numberAccountDestination },
-                    { $inc: { balance: data.amount } }, // Incrementing the balance by the new amount
-                    { new: true }
-                );
-            }
-        }  else {
-            return res.status(400).send({ message: 'You have to send the new account and new amount.' });
-        }
- 
-        return res.send({ message: 'Movement updated successfully.', updateMovement });
-    } catch (err) {
-        console.error(err);
-        return res.status(500).send({ message: 'Error updating the movement.' });
-    }
-} */
-
 export const updateMovement = async (req, res) => {
   try {
     let data = req.body;
