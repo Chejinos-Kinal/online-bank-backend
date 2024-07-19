003455e39b9832987d888b3e79d28775e2823359
Sergio Tepaz <stepaz-2022164@kinal.edu.gt>
2024-07-19 00:35:06 -0600

    Get category in routes

--- src/routes/admin/category.routes.js
+++ src/routes/admin/category.routes.js
@@ -4,11 +4,13 @@ import {
   addCategory,
   updateCategory,
   deleteCategory,
+  getCategories,
 } from '../../controllers/category.controller.js';
 
 const router = express.Router();
 
 // NOTE: categories routes
+router.get('/get/categories', getCategories);
 router.post('/add/category', addCategory);
 router.put('/update/category/:categoryId', updateCategory);
 router.delete('/delete/category/:categoryId', deleteCategory);
